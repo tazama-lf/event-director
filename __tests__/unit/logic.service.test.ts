ed8ddb8f7e8d0ae029b473f5ebfbff1aa867fbfe
rtkay123 <rodney.kanjala@sybrin.com>
2024-05-07 12:50:59 +0200

    chore(deps): bump coe-lib

--- __tests__/unit/logic.service.test.ts
+++ __tests__/unit/logic.service.test.ts
@@ -2,7 +2,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { databaseManager, dbInit, loggerService, nodeCache, runServer, server } from '../../src';
 import { handleTransaction } from '../../src/services/logic.service';
-import { Pacs008Sample, Pacs002Sample, Pain001Sample, Pain013Sample } from '@frmscoe/frms-coe-lib/lib/tests/data';
+import { Pacs008Sample, Pacs002Sample, Pain001Sample, Pain013Sample, NetworkMapSample } from '@frmscoe/frms-coe-lib/lib/tests/data';
+import { DatabaseNetworkMapMocks } from '@frmscoe/frms-coe-lib/lib/tests/mocks/mock-networkmap';
 
 beforeAll(async () => {
   await dbInit();
@@ -13,108 +14,6 @@ afterAll((done) => {
   done();
 });
 
-export const NetworkMapSample = [
-  [
-    {
-      messages: [
-        {
-          id: '001@1.0',
-          host: 'http://gateway.openfaas:8080',
-          cfg: '1.0',
-          txTp: 'pain.001.001.11',
-          typologies: [
-            {
-              id: '028@1.0',
-              host: 'http://gateway.openfaas:8080/function/off-typology-processor',
-              cfg: '1.0',
-              rules: [
-                { id: '003@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-003', cfg: '1.0' },
-                { id: '028@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-028', cfg: '1.0' },
-              ],
-            },
-          ],
-        },
-        {
-          id: '002@1.0',
-          host: 'http://gateway.openfaas:8080',
-          cfg: '1.0',
-          txTp: 'pain.013.001.09',
-          typologies: [
-            {
-              id: '028@1.0',
-              host: 'http://gateway.openfaas:8080/function/off-typology-processor',
-              cfg: '1.0',
-              rules: [
-                { id: '003@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-003', cfg: '1.0' },
-                { id: '028@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-028', cfg: '1.0' },
-              ],
-            },
-            {
-              id: '029@1.0',
-              host: 'http://gateway.openfaas:8080/function/off-typology-processor',
-              cfg: '1.0',
-              rules: [
-                { id: '003@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-003', cfg: '1.0' },
-                { id: '028@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-028', cfg: '1.0' },
-              ],
-            },
-            {
-              id: '030@1.0',
-              host: 'http://gateway.openfaas:8080/function/off-typology-processor',
-              cfg: '1.0',
-              rules: [
-                { id: '003@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-003', cfg: '1.0' },
-                { id: '028@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-028', cfg: '1.0' },
-              ],
-            },
-            {
-              id: '031@1.0',
-              host: 'http://gateway.openfaas:8080/function/off-typology-processor',
-              cfg: '1.0',
-              rules: [
-                { id: '003@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-003', cfg: '1.0' },
-                { id: '028@1.0', host: 'http://gateway.openfaas:8080/function/off-rule-028', cfg: '1.0' },
-              ],
-            },
-          ],
-        },
-        {
-          id: '004@1.0.0',
-          host: 'https://gateway.openfaas:8080/function/off-transaction-aggregation-decisioning-processor-rel-1-1-0',
-          cfg: '1.0.0',
-          txTp: 'pacs.002.001.12',
-          typologies: [
-            {
-              id: '028@1.0.0',
-              host: 'https://gateway.openfaas:8080/function/off-typology-processor-rel-1-0-0',
-              cfg: '1.0.0',
-              rules: [{ id: '018@1.0', host: 'https://gateway.openfaas:8080/function/off-rule-018-rel-1-0-0', cfg: '1.0.0' }],
-            },
-          ],
-        },
-        {
-          id: '005@1.0.0',
-          host: 'https://gateway.openfaas:8080/function/off-transaction-aggregation-decisioning-processor-rel-1-1-0',
-          cfg: '1.0.0',
-          txTp: 'pacs.008.001.10',
-          typologies: [
-            {
-              id: '028@1.0.0',
-              host: 'https://gateway.openfaas:8080/function/off-typology-processor-rel-1-0-0',
-              cfg: '1.0.0',
-              rules: [{ id: '018@1.0', host: 'https://gateway.openfaas:8080/function/off-rule-018-rel-1-0-0', cfg: '1.0.0' }],
-            },
-          ],
-        },
-      ],
-    },
-  ],
-];
-export const DatabaseNetworkMapMocks = (databaseManager: any): void => {
-  jest.spyOn(databaseManager, 'getNetworkMap').mockImplementation(async () => {
-    return await Promise.resolve(NetworkMapSample);
-  });
-};
 describe('Logic Service', () => {
   let debugLog = '';
   let loggerSpy: jest.SpyInstance;
