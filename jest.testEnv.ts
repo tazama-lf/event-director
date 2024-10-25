276e058a1cb38eb1df56db9464e12cae02955513
Jean-Pierre <Jean-Pierre.NelL@Sybrin.com>
2024-10-25 11:46:11 +0200

    test(env): singular jest instance

--- jest.testEnv.ts
+++ jest.testEnv.ts
@@ -2,7 +2,7 @@
 
 process.env.FUNCTION_NAME = 'event-director';
 process.env.NODE_ENV = 'test';
-process.env.MAX_CPU = '';
+process.env.MAX_CPU = '1';
 
 process.env.APM_ACTIVE = 'false';
 process.env.APM_SERVICE_NAME = '';
