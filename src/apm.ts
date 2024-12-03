fc69bc78a28ab89740e49ce3d833532eb6e7962d
Scott <busingescott@gmail.com>
2024-12-03 14:10:54 +0300

    feat: Release V2.1.0 (#243)
    * chore(deps-dev): bump typescript from 5.4.5 to 5.6.2
    
    Bumps [typescript](https://github.com/microsoft/TypeScript) from 5.4.5 to 5.6.2.
    - [Release notes](https://github.com/microsoft/TypeScript/releases)
    - [Changelog](https://github.com/microsoft/TypeScript/blob/main/azure-pipelines.release.yml)
    - [Commits](https://github.com/microsoft/TypeScript/compare/v5.4.5...v5.6.2)
    
    ---
    updated-dependencies:
    - dependency-name: typescript
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * feat: validate variables
    
    * test: update lib
    
    * test: mock libs
    
    * test: mock libs
    
    * fix: reuse dbconfig type
    
    * fix: update test import paths
    
    * build: update lib
    
    * refactor: apply cache option environment variable split
    
    * refactor: encapsulated the environment variables
    
    * test: refactored units to accommodate encapsulation of environment variables
    
    * refactor: update package lock file
    
    * refactor: fix typo
    
    * refactor: bump tazama libraries
    
    * refactor: remove unused mocks
    
    * refactor: bump lib version
    
    * feat: updated Version of libraries to support multi-currency
    
    * test(env): singular jest instance
    
    * build: update libs (#241)
    
    Co-authored-by: rtkay123 <rodney.kanjala@sybrin.com>
    
    * ci: sync workflows from central-workflows (#242)
    
    * ci: sync workflows from central-workflows Signed-off-by: Scott <busingescott@gmail.com>
    
    * ci: sync workflows from central-workflows Signed-off-by: Scott <busingescott@gmail.com>
    
    * ci: sync workflows from central-workflows Signed-off-by: Scott <busingescott@gmail.com>
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: rtkay123 <rodney.kanjala@sybrin.com>
    Co-authored-by: rtkay123 <70331483+rtkay123@users.noreply.github.com>
    Co-authored-by: Justus Ortlepp <123470803+Justus-at-Tazama@users.noreply.github.com>
    Co-authored-by: cshezi <cshezi@sybrin.co.za>
    Co-authored-by: Cebolenkosi Shezi <131665740+cshezi@users.noreply.github.com>
    Co-authored-by: Jean-Pierre <Jean-Pierre.NelL@Sybrin.com>
    Co-authored-by: Jean-Pierre Nell <48053940+JeanPierreNell@users.noreply.github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- src/apm.ts
+++ src/apm.ts
@@ -1,13 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
-import { Apm } from '@frmscoe/frms-coe-lib/lib/services/apm';
-import { configuration } from './config';
+import { Apm } from '@tazama-lf/frms-coe-lib/lib/services/apm';
 
 const apm = new Apm({
-  serviceName: configuration.functionName,
-  secretToken: configuration.apmSecretToken,
-  serverUrl: configuration.apmURL,
   usePathAsTransactionName: true,
-  active: configuration.apmLogging,
   transactionIgnoreUrls: ['/health'],
 });
 
--- src/config.ts
+++ src/config.ts
@@ -1,61 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
-import { type ManagerConfig } from '@frmscoe/frms-coe-lib';
-import * as dotenv from 'dotenv';
-import path from 'path';
+import { type ManagerConfig } from '@tazama-lf/frms-coe-lib';
+import { type AdditionalConfig, type ProcessorConfig } from '@tazama-lf/frms-coe-lib/lib/config/processor.config';
 
-// Load .env file into process.env if it exists. This is convenient for running locally.
-dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
-});
-
-export interface IConfig {
-  maxCPU: number;
-  db: ManagerConfig;
-  logger: {
-    logstashHost: string;
-    logstashPort: number;
-    logstashLevel: string;
-  };
-  arangoHost: string;
-  arangoPort: number;
-  functionName: string;
-  apmLogging: boolean;
-  apmSecretToken: string;
-  apmURL: string;
-  nodeEnv: string;
-  nodeCacheTTL: number;
-  sidecarHost: string;
+/**
+ * Additional environment variables are accompanied by their interface.
+ * The interface defines the key as the actual environment variable name,
+ * and the corresponding type must match the one specified within the array
+ * of additional environment variables.
+ * @example { HOST: string, PORT: number }
+ */
+interface AdditionalEnvironmentVariables {
+  ENVIRONMENTVARIABLENAME: string;
 }
 
-export const configuration: IConfig = {
-  maxCPU: parseInt(process.env.MAX_CPU!, 10) || 1,
-  db: {
-    redisConfig: {
-      db: parseInt(process.env.REDIS_DB ?? ''),
-      servers: JSON.parse(process.env.REDIS_SERVERS! || '[{"hostname": "127.0.0.1", "port":6379}]'),
-      password: process.env.REDIS_AUTH!,
-      isCluster: process.env.REDIS_IS_CLUSTER === 'true',
-    },
-    configuration: {
-      url: process.env.DATABASE_URL!,
-      databaseName: process.env.DATABASE_NAME!,
-      user: process.env.DATABASE_USER!,
-      password: process.env.DATABASE_PASSWORD!,
-      certPath: process.env.DATABASE_CERT_PATH!,
-    },
-  },
-  logger: {
-    logstashHost: process.env.LOGSTASH_HOST!,
-    logstashPort: parseInt(process.env.LOGSTASH_PORT ?? '0', 10),
-    logstashLevel: process.env.LOGSTASH_LEVEL! || 'info',
-  },
-  arangoHost: process.env.ARANGO_HOST!,
-  arangoPort: parseInt(process.env.arangoPort ?? '', 10),
-  functionName: process.env.FUNCTION_NAME!,
-  apmLogging: process.env.APM_ACTIVE === 'true',
-  apmSecretToken: process.env.APM_SECRET_TOKEN!,
-  apmURL: process.env.APM_URL!,
-  nodeEnv: process.env.NODE_ENV!,
-  nodeCacheTTL: parseInt(process.env.CACHETTL ?? '300', 10),
-  sidecarHost: process.env.SIDECAR_HOST!,
-};
+export const additionalEnvironmentVariables: AdditionalConfig[] = [];
+
+export type Configuration = ProcessorConfig & ManagerConfig & AdditionalEnvironmentVariables;
